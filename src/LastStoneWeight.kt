8e69bea211420249fbe5d938e74968b0b3a757ef
pocorusso <pocorusso@gmail.com>
2020-04-12 13:26:01 +0200

    last stone weight faster

--- src/LastStoneWeight.kt
+++ src/LastStoneWeight.kt
@@ -1,25 +1,29 @@
+import java.util.Collections
+import java.util.PriorityQueue
+
 class LastStoneWeight {
     fun run() {
         println(lastStoneWeight(intArrayOf(2, 2)))
         println(lastStoneWeight(intArrayOf(2, 7, 4, 1, 8, 1)))
         println(lastStoneWeight(intArrayOf(1)))
-        println(lastStoneWeight(intArrayOf(1,3,3,2,1,1,1,1,1,1)))
+        println(lastStoneWeight(intArrayOf(1, 3, 3, 2, 1, 1, 1, 1, 1, 1)))
     }
 
     fun lastStoneWeight(stones: IntArray): Int {
-        return smash(stones.toMutableList())
+        val pq = PriorityQueue<Int>(Collections.reverseOrder())
+        stones.forEach { pq.offer(it) }
+        return smash(pq)
     }
 
-    fun smash(stones: MutableList<Int>) : Int {
+    fun smash(stones: PriorityQueue<Int>): Int {
         if (stones.size == 0) return 0
-        if (stones.size == 1) return stones[0]
+        if (stones.size == 1) return stones.poll()
 
-        stones.sort()
-        val end = stones.size - 1
-        val diff = stones[end] - stones[end - 1]
-        stones.removeAt(end)
-        if (diff == 0) stones.removeAt(end - 1) else stones[end - 1] = diff
+        val end1 = stones.poll()
+        val end2 = stones.poll()
+        val diff = end1 - end2
+
+        if (diff > 0) stones.offer(diff)
         return smash(stones)
     }
-
 }
